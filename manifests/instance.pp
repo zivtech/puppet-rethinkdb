59fbbd8400c0b39a92e2b178afab42ee3154530a
Howard Tyson <howard@howardtyson.com>
2015-06-02 13:40:56 -0400

    Fixing default on an instance for the case where it is not specified.

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,11 +1,11 @@
 define rethinkdb::instance (
   $custom_server_name = false,
-  $conf = false,
+  $conf = {},
   $ensure = 'present'
 ) {
 
   validate_hash($conf)
-  
+
   file { "rethinkdb-instance-${name}":
     path    => "${rethinkdb::instance_path}/${name}.conf",
     content => template("${module_name}/instance.conf.erb"),
