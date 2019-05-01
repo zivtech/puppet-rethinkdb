76b17da20ecd4be0ac5f594fb46483ef34740c1f
Matt <matt@zivtech.com>
2019-04-30 21:13:26 -0400

    some more fixes

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,53 +11,43 @@ class rethinkdb::install inherits rethinkdb {
     home       => '/var/lib/rethinkdb',
     membership => 'minimum',
   }->
-  file { '/etc/rethinkdb':
-    owner  => $user,
-    group  => $group,
-    mode   => '0755',
-    ensure => 'directory',
-  }->
-  file { $::rethinkdb::instance_path:
-    owner  => $user,
-    group  => $group,
-    mode   => '0755',
-    ensure => 'directory',
-  }->
   file { '/var/lib/rethinkdb':
     owner  => $user,
     group  => $group,
     mode   => '0755',
     ensure => 'directory',
-  }->
+  }
+
   wget::fetch { "${fetch_url}":
     destination => "/tmp/rethinkdb.tgz",
     # Currently, cache_dir doesn't work with source_hash.
     # https://github.com/voxpupuli/puppet-wget/issues/87
     #cache_dir   => '/var/cache/rethinkdb',
-    unless      => "/usr/bin/test -f /usr/bin/rethinkdb",
+    #unless      => "/usr/bin/test /tmp/rethinkdb",
+    redownload  => false,
     source_hash => $checksum,
     timeout     => 0,
     verbose     => false,
-  }->
+  }~>
   exec { "uncompress /tmp/rethinkdb.tgz":
-    command => "/bin/tar xzf /tmp/rethinkdb.tgz",
+    command => "/bin/tar -xzf /tmp/rethinkdb.tgz",
     cwd     => '/tmp',
     creates => "/tmp/rethinkdb",
-  }->
+  }~>
   file { '/usr/bin/rethinkdb':
     ensure  => present,
     source  => "/tmp/rethinkdb",
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
-  }->
+  }~>
   file { '/usr/lib/tmpfiles.d/rethinkdb.conf':
     ensure  => present,
     content => 'd /run/rethinkdb 0755 rethinkdb rethinkdb -',
     owner   => $user,
     group   => $group,
     mode    => '0644',
-  }->
+  }~>
   file { '/lib/systemd/system/rethinkdb@.service':
     ensure  => present,
     content => template("${module_name}/rethinkdb.service.erb"),
